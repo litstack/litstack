04bd7ba2d3943fa621676b5dd5f8fa298868c1aa
Jannes Carstens-Behrens <jannescb@me.com>
2019-10-16 10:52:36 +0200

    crud index table action transition

--- resources/assets/components/Indexes/SelectedItemsActions.vue
+++ resources/assets/components/Indexes/SelectedItemsActions.vue
@@ -1,36 +1,37 @@
 <template>
-    <b-input-group
-        v-if="selectedItems.length > 0"
-        :size="'sm'"
-        class="mt-3"
-    >
-        <b-input-group-prepend is-text>
-            <strong>
-                {{ selectedItems.length }}
-                {{
-                    selectedItems.length == 1
-                        ? ' Item'
-                        : ' Items'
-                }}
-                {{
-                    selectedItems.length == items.length
-                        ? ' (all)'
-                        : ''
-                }}
-                selected
-            </strong>
-        </b-input-group-prepend>
+    <transition name="slide">
+        <b-input-group
+            v-if="selectedItems.length > 0"
+            :size="'sm'"
+        >
+            <b-input-group-prepend is-text>
+                <strong>
+                    {{ selectedItems.length }}
+                    {{
+                        selectedItems.length == 1
+                            ? ' Item'
+                            : ' Items'
+                    }}
+                    {{
+                        selectedItems.length == items.length
+                            ? ' (all)'
+                            : ''
+                    }}
+                    selected
+                </strong>
+            </b-input-group-prepend>
 
-        <template v-slot:append>
-            <b-dropdown
-                text="Actions"
-                class="btn-brl-none"
-                variant="outline-secondary"
-            >
-                <slot name="actions" />
-            </b-dropdown>
-        </template>
-    </b-input-group>
+            <template v-slot:append>
+                <b-dropdown
+                    text="Actions"
+                    class="btn-brl-none"
+                    variant="outline-secondary"
+                >
+                    <slot name="actions" />
+                </b-dropdown>
+            </template>
+        </b-input-group>
+    </transition>
 </template>
 
 <script>
@@ -48,3 +49,37 @@ export default {
     }
 }
 </script>
+
+<style lang="css" scoped>
+.slide-enter-active {
+   -moz-transition-duration: 0.3s;
+   -webkit-transition-duration: 0.3s;
+   -o-transition-duration: 0.3s;
+   transition-duration: 0.3s;
+   -moz-transition-timing-function: ease-in;
+   -webkit-transition-timing-function: ease-in;
+   -o-transition-timing-function: ease-in;
+   transition-timing-function: ease-in;
+}
+
+.slide-leave-active {
+   -moz-transition-duration: 0.3s;
+   -webkit-transition-duration: 0.3s;
+   -o-transition-duration: 0.3s;
+   transition-duration: 0.3s;
+   -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+   -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+   -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+   transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+}
+
+.slide-enter-to, .slide-leave {
+   max-height: 100px;
+   overflow: hidden;
+}
+
+.slide-enter, .slide-leave-to {
+   overflow: hidden;
+   max-height: 0;
+}
+</style>
