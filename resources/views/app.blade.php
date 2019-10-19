fcd58abad6e54f23f8df4c503993ff0031e2ae08
Jannes Behrens <jannescb@me.com>
2019-10-19 22:16:55 +0200

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="{{route('fjord.favicon-big')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{route('fjord.favicon-small')}}">
+
     <!-- Styles -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
 
