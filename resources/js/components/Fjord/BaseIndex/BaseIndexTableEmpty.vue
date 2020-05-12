6ac8b26ff86660a4a101b31859abc6ab31f993a7
cbl <lennart.carbe@gmail.com>
2020-05-12 18:15:32 +0200

    empty table icon

--- resources/js/components/Fjord/BaseIndex/BaseIndexTableEmpty.vue
+++ resources/js/components/Fjord/BaseIndex/BaseIndexTableEmpty.vue
@@ -2,7 +2,7 @@
     <tr class="fj-table-row-no-hover">
         <td :colspan="colspan" class="text-center text-secondary">
             <div class="my-2">
-                <fa-icon icon="box-open" size="5x" />
+                <fa-icon icon="robot" size="5x" />
             </div>
             <div>
                 {{ __('messages.no_items_found', { items: namePlural }) }}
