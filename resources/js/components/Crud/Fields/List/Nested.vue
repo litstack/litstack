e6ffd9bc901e9ef33d1c0fb4056a2d0e77e4d019
Jannes <jannescb@me.com>
2020-06-09 18:46:36 +0200

    tests for list form field

--- resources/js/components/Crud/Fields/List/Nested.vue
+++ resources/js/components/Crud/Fields/List/Nested.vue
@@ -1,13 +1,21 @@
 <template>
-    <draggable class="dragArea" tag="ul" :list="list" :group="{ name: 'g1' }">
-        <li v-for="el in list" :key="el.id" class="">
+    <draggable
+        class="fj-list"
+        handle=".fj-list__dragbar"
+        tag="ul"
+        :list="children"
+        :group="{ name: 'g1' }"
+    >
+        <li v-for="el in children" :key="el.id" class="">
             <div class="fj-list-entry fj-block mb-2">
-                <i class="fas fa-grip-horizontal text-secondary"></i>
+                <i
+                    class="fas fa-grip-horizontal text-secondary fj-list__dragbar"
+                ></i>
                 <div class="pl-2 d-inline-block">
                     {{ el.title }}
                 </div>
             </div>
-            <nested-draggable :list="el.list" />
+            <nested-draggable :children="el.children" />
         </li>
     </draggable>
 </template>
@@ -16,7 +24,7 @@ import draggable from 'vuedraggable';
 export default {
     name: 'nested-draggable',
     props: {
-        list: {
+        children: {
             required: true,
             type: Array
         }
@@ -28,15 +36,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-ul.dragArea {
+ul.fj-list {
+    width: 100%;
     padding: 0;
     list-style-type: none;
 
-    ul.dragArea {
+    ul.fj-list {
         padding-left: 2rem;
     }
 }
 .fj-list-entry {
     padding: 1rem;
+}
+.fj-list__dragbar {
+    cursor: grab;
 }
 </style>
