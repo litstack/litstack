ba1792c4badd81de6cea326a4be11aab30e96283
cbl <lennart.carbe@gmail.com>
2020-06-10 11:59:29 +0200

    docs dependencies

--- publish/fjord/app/Config/NavigationConfig.php
+++ publish/fjord/app/Config/NavigationConfig.php
@@ -22,7 +22,7 @@ class NavigationConfig extends Config
         $nav->section([
             $nav->title(__f('fj.user_administration')),
 
-            $nav->preset('user.user_index'),
+            $nav->preset('user.user'),
             $nav->preset('permissions')
         ]);
 
