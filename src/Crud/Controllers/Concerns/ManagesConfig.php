3771aa35e0166ebfdd90131f39a6e0c8a06d25af
cbl <lennart.carbe@gmail.com>
2020-07-06 17:01:53 +0200

    docblocks

--- src/Crud/Controllers/Concerns/ManagesConfig.php
+++ src/Crud/Controllers/Concerns/ManagesConfig.php
@@ -9,14 +9,14 @@ trait ManagesConfig
     /**
      * Config instance.
      *
-     * @var Instance
+     * @var \Fjord\Config\ConfigHandler
      */
     protected $config;
 
     /**
      * Get config.
      *
-     * @return Config
+     * @return \Fjord\Config\ConfigHandler
      */
     protected function loadConfig()
     {
@@ -27,6 +27,11 @@ trait ManagesConfig
         return $route->getConfig();
     }
 
+    /**
+     * Get crud/form config.
+     *
+     * @return \Fjord\Config\ConfigHandler
+     */
     public function getConfig()
     {
         return $this->config;
