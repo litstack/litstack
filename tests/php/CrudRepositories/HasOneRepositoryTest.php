0cdffe342f8517ad07f175fafd2e610460311974
cbl <lennart.carbe@gmail.com>
2020-07-06 15:16:21 +0200

    tests

---
+++ tests/php/CrudRepositories/HasOneRepositoryTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace FjordTest\CrudRepositories;
+
+use Fjord\Config\ConfigHandler;
+use Fjord\Crud\Api\ApiRepositories;
+use Fjord\Crud\BaseForm;
+use Fjord\Crud\Controllers\CrudController;
+use Fjord\Crud\Fields\Relations\HasOne;
+use Fjord\Crud\Repositories\Relations\HasOneRepository;
+use Fjord\Crud\Requests\CrudUpdateRequest;
+use FjordTest\BackendTestCase;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Mockery as m;
+
+/**
+ * @see https://laravel.com/docs/7.x/eloquent-relationships#one-to-one
+ */
+class HasOneRepositoryTest extends BackendTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Schema::create('users', fn ($table) => $table->id());
+        Schema::create('phone', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->default(null)->nullable();
+        });
+
+        $config = m::mock(ConfigHandler::class);
+        $controller = m::mock(CrudController::class);
+        $form = m::mock(BaseForm::class);
+        $this->field = m::mock(HasOne::class);
+        $this->repository = new HasOneRepository($config, $controller, $form, $this->field);
+    }
+
+    public function tearDown(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('phone');
+    }
+
+    /** @test */
+    public function test_create_method()
+    {
+        $user = HasOneRepositoryUser::create();
+        $phone = HasOneRepositoryPhone::create();
+
+        $request = m::mock(CrudUpdateRequest::class);
+        $request->related_id = $phone->id;
+
+        $this->field->shouldReceive('getQuery')->andReturn($phone->query());
+        $this->field->shouldReceive('getRelationQuery')->andReturn($user->phone());
+        $this->repository->create($request, $user);
+
+        $this->assertEquals($phone->id, $user->refresh()->phone->id);
+    }
+
+    /** @test */
+    public function test_destroy_method()
+    {
+        $user = HasOneRepositoryUser::create();
+        $phone = HasOneRepositoryPhone::create([
+            'user_id' => $user->id,
+        ]);
+
+        $request = m::mock(CrudUpdateRequest::class);
+        $request->related_id = $phone->id;
+
+        $this->field->shouldReceive('getQuery')->andReturn($phone->query());
+        $this->field->shouldReceive('getRelationQuery')->andReturn($user->phone());
+        $this->repository->destroy($request, $user);
+
+        $this->assertNull($user->refresh()->phone);
+    }
+
+    /** @test */
+    public function test_repository_is_registered()
+    {
+        $this->assertEquals(
+            HasOneRepository::class,
+            app(ApiRepositories::class)->find('has-one')
+        );
+    }
+}
+
+class HasOneRepositoryUser extends Model
+{
+    public $table = 'users';
+    public $timestamps = false;
+
+    public function phone()
+    {
+        return $this->hasOne(HasOneRepositoryPhone::class, 'user_id');
+    }
+}
+class HasOneRepositoryPhone extends Model
+{
+    public $table = 'phone';
+    public $timestamps = false;
+    protected $fillable = ['user_id'];
+}
