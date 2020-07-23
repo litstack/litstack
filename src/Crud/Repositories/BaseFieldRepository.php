81e4c8aef5af5cfe191e014a8577a7e464ef78c6
cbl <lennart.carbe@gmail.com>
2020-07-23 16:31:19 +0200

    wip

--- src/Crud/Repositories/BaseFieldRepository.php
+++ src/Crud/Repositories/BaseFieldRepository.php
@@ -63,11 +63,7 @@ abstract class BaseFieldRepository
             }
 
             $field->fillModel($model, $field->local_key, $attributes[$field->local_key]);
-
-            unset($attributes[$field->local_key]);
         }
-
-        return $attributes;
     }
 
     protected function getRegisteredFields()
