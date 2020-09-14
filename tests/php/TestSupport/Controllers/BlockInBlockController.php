c2ba1bc5fc3a1336fa8fb9eca5b0bd7b9ff69839
cbl <lennart.carbe@gmail.com>
2020-09-14 14:58:27 +0200

    fix

--- tests/php/TestSupport/Controllers/BlockInBlockController.php
+++ tests/php/TestSupport/Controllers/BlockInBlockController.php
@@ -3,25 +3,12 @@
 namespace Tests\TestSupport\Controllers;
 
 use Ignite\Crud\Controllers\CrudController;
-use Illuminate\Database\Eloquent\Builder;
 use Lit\Models\User;
 
 class BlockInBlockController extends CrudController
 {
-    /**
-     * Crud model class name.
-     *
-     * @var string
-     */
-    protected $model = \Tests\TestSupport\Models\Post::class;
-
     public function authorize(User $user, string $operation): bool
     {
         return true;
-    }
-
-    public function query(): Builder
-    {
-        return $this->model::query();
     }
 }
--- tests/php/TestSupport/Controllers/PostController.php
+++ tests/php/TestSupport/Controllers/PostController.php
@@ -3,18 +3,10 @@
 namespace Tests\TestSupport\Controllers;
 
 use Ignite\Crud\Controllers\CrudController;
-use Illuminate\Database\Eloquent\Builder;
 use Lit\Models\User;
 
 class PostController extends CrudController
 {
-    /**
-     * Crud model class name.
-     *
-     * @var string
-     */
-    protected $model = \Tests\TestSupport\Models\Post::class;
-
     /**
      * Authorize request for permission operation and authenticated lit-user.
      * Operations: create, read, update, delete.
@@ -27,15 +19,5 @@ class PostController extends CrudController
     public function authorize(User $user, string $operation): bool
     {
         return true;
-    }
-
-    /**
-     * Initial query.
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function query(): Builder
-    {
-        return $this->model::query();
     }
 }
