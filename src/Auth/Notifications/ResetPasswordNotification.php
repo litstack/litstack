ee6aec95c53bfccc81ea3e6f3a1ef554cea468c1
cbl <lennart.carbe@gmail.com>
2020-09-17 13:13:00 +0200

    housekeeping

--- src/Auth/Notifications/ResetPasswordNotification.php
+++ src/Auth/Notifications/ResetPasswordNotification.php
@@ -25,8 +25,7 @@ class ResetPasswordNotification extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param mixed $notifiable
-     *
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -37,8 +36,7 @@ class ResetPasswordNotification extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param mixed $notifiable
-     *
+     * @param  mixed                                          $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
@@ -51,8 +49,7 @@ class ResetPasswordNotification extends Notification
     /**
      * Get the array representation of the notification.
      *
-     * @param mixed $notifiable
-     *
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
