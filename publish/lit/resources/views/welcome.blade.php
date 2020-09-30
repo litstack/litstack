5a6386974fa5f72f3edfc669a8f0632d9f311b76
cbl <lennart.carbe@gmail.com>
2020-09-30 17:19:38 +0200

    welcome

--- publish/lit/resources/views/welcome.blade.php
+++ publish/lit/resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <lit-col width="12">
-    <svg class="mb-5" width="100%" height="30vh" viewBox="0 0 169 263" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <svg width="100%" height="30vh" viewBox="0 0 169 263" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;margin-top: 16vh;">
         <g transform="matrix(1,0,0,1,-204.96,-204.96)">
             <path d="M204.96,368.93C204.96,391.57 223.31,409.92 245.95,409.92C268.59,409.92 286.94,391.57 286.94,368.93L286.94,245.95L204.96,245.95L204.96,368.93Z" style="fill:rgb(73,81,242);fill-rule:nonzero;"/>
             <path d="M204.96,327.94C204.96,350.58 223.31,368.93 245.95,368.93C268.59,368.93 286.94,350.58 286.94,327.94L286.94,245.96L204.96,245.96L204.96,327.94Z" style="fill:rgb(64,255,164);fill-opacity:0.42;fill-rule:nonzero;"/>
