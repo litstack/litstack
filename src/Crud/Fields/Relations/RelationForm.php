f7b86496cf83146c9bc46428eab42c33643de576
cbl <lennart.carbe@gmail.com>
2020-10-25 17:54:01 +0100

    wip

--- src/Crud/Fields/Relations/RelationForm.php
+++ src/Crud/Fields/Relations/RelationForm.php
@@ -14,6 +14,16 @@ class RelationForm extends BaseForm
     protected $field;
 
     /**
+     * Determine's what the form can be used for.
+     *
+     * @var array
+     */
+    protected $allow = [
+        'create' => true,
+        'update' => true,
+    ];
+
+    /**
      * Create new RelationForm instance.
      *
      * @param  string               $model
@@ -44,5 +54,64 @@ class RelationForm extends BaseForm
     public function pivot()
     {
         return new PivotField($this);
+    }
+
+    /**
+     * Allow using form for updating a connected related model.
+     *
+     * @param  bool  $allow
+     * @return $this
+     */
+    public function allowUpdate(bool $allow = true)
+    {
+        $this->allow['update'] = $allow;
+
+        return $this;
+    }
+
+    /**
+     * Determine if the form can be used for updating a Model.
+     *
+     * @return bool
+     */
+    public function allowsUpdate(): bool
+    {
+        return $this->allow['update'];
+    }
+
+    /**
+     * Allow using form for creating a new related model.
+     *
+     * @param  bool  $allow
+     * @return $this
+     */
+    public function allowCreate(bool $allow = true)
+    {
+        $this->allow['create'] = $allow;
+
+        return $this;
+    }
+
+    /**
+     * Determine if the form can be used for Model creation.
+     *
+     * @return bool
+     */
+    public function allowsCreate(): bool
+    {
+        return $this->allow['create'];
+    }
+
+    /**
+     * Render RelationForm.
+     *
+     * @return array
+     */
+    public function render(): array
+    {
+        $rendered = parent::render();
+        $rendered['allow'] = $this->allow;
+
+        return $rendered;
     }
 }
