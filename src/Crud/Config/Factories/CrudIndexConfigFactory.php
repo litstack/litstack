fabfb17f1814bced7f7ac3376bfd5ad52f6bcd36
cbl <lennart.carbe@gmail.com>
2020-10-26 17:08:49 +0100

    hide create when unauthorized

--- src/Crud/Config/Factories/CrudIndexConfigFactory.php
+++ src/Crud/Config/Factories/CrudIndexConfigFactory.php
@@ -22,12 +22,12 @@ class CrudIndexConfigFactory extends ConfigFactory
     {
         $page = new CrudIndex($config);
 
-        if ($config->has('show')) {
+        if ($this->canCreate($config)) {
             $page->navigationRight()
                 ->component('b-button')
                 ->variant('primary')
                 ->child($this->createButtonText($config))
-                ->prop('href', lit()->url($config->routePrefix().'/create'));
+                ->prop('href', lit()->url($config->routePrefix() . '/create'));
         }
 
         $page->title($config->names['plural'] ?? '');
@@ -37,6 +37,18 @@ class CrudIndexConfigFactory extends ConfigFactory
         $method($page);
 
         return $page;
+    }
+
+    /**
+     * Determines if a user can create a  new crud model.
+     *
+     * @param  ConfigHandler $config
+     * @return bool
+     */
+    protected function canCreate(ConfigHandler $config)
+    {
+        return $config->has('show')
+            && $config->authorize(lit_user(), 'create');
     }
 
     /**
