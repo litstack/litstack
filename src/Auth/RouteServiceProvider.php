273a9c963fb8b1458319e38592c95b8378e7fc4f
Lukas Heller <mail@lukas-heller.de>
2021-01-21 14:04:02 +0100

    public reset password routes

--- src/Auth/RouteServiceProvider.php
+++ src/Auth/RouteServiceProvider.php
@@ -25,6 +25,9 @@ class RouteServiceProvider extends LaravelRouteServiceProvider
      */
     protected function mapAuthRoutes()
     {
+        Route::public()->get('password/reset/{token}', ResetPasswordController::class.'@showResetForm')->name('password.reset');
+        Route::public()->post('password/reset', ResetPasswordController::class.'@request')->name('password.request');
+
         Route::public()->get('login', AuthController::class.'@login')->name('login');
         Route::public()->post('login', AuthController::class.'@authenticate')->name('login.post');
         Route::post('logout', AuthController::class.'@logout')->name('logout');
