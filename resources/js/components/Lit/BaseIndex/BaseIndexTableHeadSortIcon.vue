4cead3652f6f078d42531092f2562f8b5d5bd787
cbl <lennart.carbe@gmail.com>
2021-01-28 22:38:00 +0100

    fix sort icon

---
+++ resources/js/components/Lit/BaseIndex/BaseIndexTableHeadSortIcon.vue
@@ -0,0 +1,23 @@
+<template>
+    <span v-html="icon"> </span>
+</template>
+
+<script>
+export default {
+    name: 'BaseIndexTableHeadSortIcon',
+    props: {
+        sort: {
+            type: String,
+        },
+    },
+    watch: {
+        sort() {
+            console.log({ sort: this.sort });
+            this.$nextTick(() => {
+                this.$forceUpdate();
+            });
+        },
+    },
+    computed: {},
+};
+</script>
