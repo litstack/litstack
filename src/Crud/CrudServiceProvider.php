295cd89e3373c17045e4433dae1cc04598de7257
Jannes <jannescb@me.com>
2021-03-02 09:34:10 +0100

    added macro command

--- src/Crud/CrudServiceProvider.php
+++ src/Crud/CrudServiceProvider.php
@@ -2,63 +2,64 @@
 
 namespace Ignite\Crud;
 
-use Ignite\Crud\Api\ApiRepositories;
-use Ignite\Crud\Config\CrudConfig;
-use Ignite\Crud\Config\Factories\CrudConfigFactory;
-use Ignite\Crud\Config\Factories\CrudFormConfigFactory;
-use Ignite\Crud\Config\Factories\CrudIndexConfigFactory;
-use Ignite\Crud\Config\Traits\HasCrudIndex;
-use Ignite\Crud\Config\Traits\HasCrudShow;
-use Ignite\Crud\Console\CrudCommand;
-use Ignite\Crud\Console\FieldCommand;
-use Ignite\Crud\Console\FilterCommand;
-use Ignite\Crud\Console\FormCommand;
-use Ignite\Crud\Console\RepeatableCommand;
-use Ignite\Crud\Fields\Block\Block;
-use Ignite\Crud\Fields\Boolean;
-use Ignite\Crud\Fields\Checkboxes;
-use Ignite\Crud\Fields\Component;
-use Ignite\Crud\Fields\DateTime\Date;
-use Ignite\Crud\Fields\DateTime\DateTime;
-use Ignite\Crud\Fields\DateTime\Time;
 use Ignite\Crud\Fields\Icon;
 use Ignite\Crud\Fields\Input;
-use Ignite\Crud\Fields\ListField\ListField;
-use Ignite\Crud\Fields\Listing;
-use Ignite\Crud\Fields\Media\File;
-use Ignite\Crud\Fields\Media\Image;
 use Ignite\Crud\Fields\Modal;
-use Ignite\Crud\Fields\Password;
 use Ignite\Crud\Fields\Radio;
 use Ignite\Crud\Fields\Range;
-use Ignite\Crud\Fields\Relations\ManyRelation;
-use Ignite\Crud\Fields\Relations\OneRelation;
 use Ignite\Crud\Fields\Route;
-use Ignite\Crud\Fields\Route\RouteCollectionResolver;
 use Ignite\Crud\Fields\Select;
-use Ignite\Crud\Fields\Textarea;
+use Illuminate\Routing\Router;
+use Ignite\Crud\Fields\Boolean;
+use Ignite\Crud\Fields\Listing;
 use Ignite\Crud\Fields\Wysiwyg;
-use Ignite\Crud\Models\Relations\CrudRelations;
-use Ignite\Crud\Repositories\BlockRepository;
-use Ignite\Crud\Repositories\DefaultRepository;
+use Ignite\Crud\Fields\Password;
+use Ignite\Crud\Fields\Textarea;
+use Ignite\Crud\Fields\Component;
+use Ignite\Crud\Config\CrudConfig;
+use Ignite\Crud\Fields\Checkboxes;
+use Ignite\Crud\Fields\Media\File;
+use Ignite\Crud\Fields\Block\Block;
+use Ignite\Crud\Fields\Media\Image;
+use Ignite\Crud\Api\ApiRepositories;
+use Ignite\Crud\Console\CrudCommand;
+use Ignite\Crud\Console\FormCommand;
+use Ignite\Crud\Console\FieldCommand;
+use Ignite\Crud\Console\MacroCommand;
+use Ignite\Crud\Fields\DateTime\Date;
+use Ignite\Crud\Fields\DateTime\Time;
+use Ignite\Crud\Console\FilterCommand;
+use Illuminate\Foundation\AliasLoader;
+use Ignite\Crud\Fields\DateTime\DateTime;
+use Ignite\Crud\Config\Traits\HasCrudShow;
+use Ignite\Crud\Console\RepeatableCommand;
+use Ignite\Crud\Config\Traits\HasCrudIndex;
+use Ignite\Crud\Fields\ListField\ListField;
 use Ignite\Crud\Repositories\ListRepository;
+use Ignite\Crud\Fields\Relations\OneRelation;
+use Ignite\Crud\Repositories\BlockRepository;
 use Ignite\Crud\Repositories\MediaRepository;
 use Ignite\Crud\Repositories\ModalRepository;
-use Ignite\Crud\Repositories\RelationRepository;
-use Ignite\Crud\Repositories\Relations\BelongsToManyRepository;
-use Ignite\Crud\Repositories\Relations\BelongsToRepository;
-use Ignite\Crud\Repositories\Relations\HasManyRepository;
-use Ignite\Crud\Repositories\Relations\HasOneRepository;
-use Ignite\Crud\Repositories\Relations\ManyRelationRepository;
-use Ignite\Crud\Repositories\Relations\MorphManyRepository;
-use Ignite\Crud\Repositories\Relations\MorphOneRepository;
-use Ignite\Crud\Repositories\Relations\MorphToManyRepository;
-use Ignite\Crud\Repositories\Relations\MorphToRepository;
-use Ignite\Crud\Repositories\Relations\OneRelationRepository;
-use Ignite\Crud\Vue\FieldWrapperGroupComponent;
+use Ignite\Crud\Fields\Relations\ManyRelation;
 use Ignite\Support\Facades\Form as FormFacade;
-use Illuminate\Foundation\AliasLoader;
-use Illuminate\Routing\Router;
+use Ignite\Crud\Models\Relations\CrudRelations;
+use Ignite\Crud\Repositories\DefaultRepository;
+use Ignite\Crud\Vue\FieldWrapperGroupComponent;
+use Ignite\Crud\Repositories\RelationRepository;
+use Ignite\Crud\Config\Factories\CrudConfigFactory;
+use Ignite\Crud\Fields\Route\RouteCollectionResolver;
+use Ignite\Crud\Config\Factories\CrudFormConfigFactory;
+use Ignite\Crud\Config\Factories\CrudIndexConfigFactory;
+use Ignite\Crud\Repositories\Relations\HasOneRepository;
+use Ignite\Crud\Repositories\Relations\HasManyRepository;
+use Ignite\Crud\Repositories\Relations\MorphToRepository;
+use Ignite\Crud\Repositories\Relations\MorphOneRepository;
+use Ignite\Crud\Repositories\Relations\BelongsToRepository;
+use Ignite\Crud\Repositories\Relations\MorphManyRepository;
+use Ignite\Crud\Repositories\Relations\MorphToManyRepository;
+use Ignite\Crud\Repositories\Relations\OneRelationRepository;
+use Ignite\Crud\Repositories\Relations\ManyRelationRepository;
+use Ignite\Crud\Repositories\Relations\BelongsToManyRepository;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
 class CrudServiceProvider extends LaravelServiceProvider
@@ -116,6 +117,7 @@ class CrudServiceProvider extends LaravelServiceProvider
         'Field'      => 'lit.command.field',
         'Filter'     => 'lit.command.filters',
         'Repeatable' => 'lit.command.repeatable',
+        'Macro'      => 'lit.command.macro',
     ];
 
     /**
@@ -257,6 +259,19 @@ class CrudServiceProvider extends LaravelServiceProvider
     {
         $this->app->singleton($abstract, function ($app) {
             return new RepeatableCommand($app['files'], $app['lit']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @param  string $abstract
+     * @return void
+     */
+    protected function registerMacroCommand($abstract)
+    {
+        $this->app->singleton($abstract, function ($app) {
+            return new MacroCommand($app['files'], $app['lit']);
         });
     }
 
