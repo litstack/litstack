e7687aba9f7d9da7c1dc09ca2e508586dd471968
cbl <lennart.carbe@gmail.com>
2021-03-16 10:44:45 +0100

    test

--- src/Crud/Models/Traits/Translatable.php
+++ src/Crud/Models/Traits/Translatable.php
@@ -19,18 +19,18 @@ trait Translatable
     /**
      * Retrieve the model for a translated, bound value.
      *
-     * @param  mixed                               $value
-     * @param  string|null                         $field
-     * @return \Illuminate\Database\Eloquent\Model
+     * @param  mixed                                    $value
+     * @param  string|null                              $field
+     * @return \Illuminate\Database\Eloquent\Model|null
      */
     public function resolveRouteBinding($value, $field = null)
     {
         $field = $field ?? $this->getRouteKeyName();
 
         if (in_array($field, $this->translatedAttributes)) {
-            return $this->whereTranslation($field, $value, app()->getLocale())->firstOrFail();
+            return $this->whereTranslation($field, $value, app()->getLocale())->first();
         }
 
-        return $this->where($field, $value)->firstOrFail();
+        return $this->where($field, $value)->first();
     }
 }
