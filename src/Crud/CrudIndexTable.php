43abeef0cb54f43ca36fdb4ca2d699a29f5e9fcf
Jannes Behrens <jannescb@me.com>
2022-04-29 09:27:39 +0200

    style ci

--- src/Crud/CrudIndexTable.php
+++ src/Crud/CrudIndexTable.php
@@ -33,8 +33,8 @@ class CrudIndexTable extends Table
     /**
      * Create new CrudIndexTable instance.
      *
-     * @param  ConfigHandler     $config
-     * @param  CrudColumnBuilder $builder
+     * @param  ConfigHandler  $config
+     * @param  CrudColumnBuilder  $builder
      * @return void
      */
     public function __construct(ConfigHandler $config, CrudColumnBuilder $builder)
@@ -47,8 +47,8 @@ class CrudIndexTable extends Table
     /**
      * Add action.
      *
-     * @param  string $title
-     * @param  string $action
+     * @param  string  $title
+     * @param  string  $action
      * @return $this
      */
     public function action($title, $action)
@@ -63,7 +63,7 @@ class CrudIndexTable extends Table
     /**
      * Set table filters.
      *
-     * @param  array $filter
+     * @param  array  $filter
      * @return $this
      */
     public function filter(array $filter)
